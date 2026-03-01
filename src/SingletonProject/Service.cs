c4d68090d8b31d1e3e0d75efe737ba79ee44c98b
nikita-solomientsev-knu <nikita.solomientsev@knu.ua>
2026-03-01 19:06:28 +0200

    Singleton pattern (#5)

---
+++ src/SingletonProject/Service.cs
@@ -0,0 +1,31 @@
+﻿namespace SingletonProject;
+
+using System.Threading;
+
+
+public sealed class Service {
+
+    private static readonly Service _instance = new Service();
+    private volatile bool _isWorking = false;
+    private readonly object _workingResource = new object();
+
+    private Service() {}
+
+    public static Service GetInstance() {
+        return _instance;
+    }
+
+    public void DoWork(string description) {
+        lock (_workingResource) {
+            _isWorking = true;
+            Console.WriteLine($"Working...:\n{description}");
+            Thread.Sleep(2000);
+            _isWorking = false;
+        }
+    }
+
+    public string GetStatus() {
+        return _isWorking? "Working": "Idle";
+    }
+
+}
