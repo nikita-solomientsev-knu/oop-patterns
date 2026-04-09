dbd5c4af5bc6a66a62d86468bbe2f4d870069605
nikita-solomientsev-knu <nikita.solomientsev@knu.ua>
2026-04-09 21:44:26 +0300

    Command (#14)

---
+++ src/CommandProject/Editor.cs
@@ -0,0 +1,84 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+// Receiver
+namespace CommandProject {
+    public class TextEditor {
+        public string Text { get; private set; } = "";
+
+        public void Insert(string text) {
+            Text += text;
+        }
+
+        public void Delete(int length) {
+            if (length > Text.Length)
+                length = Text.Length;
+
+            Text = Text.Substring(0, Text.Length - length);
+        }
+    }
+
+    public interface ICommand {
+        void Execute();
+        void Undo();
+    }
+
+    public class InsertCommand : ICommand {
+        private TextEditor _editor;
+        private string _text;
+
+        public InsertCommand(TextEditor editor, string text) {
+            _editor = editor;
+            _text = text;
+        }
+
+        public void Execute() {
+            _editor.Insert(_text);
+        }
+
+        public void Undo() {
+            _editor.Delete(_text.Length);
+        }
+    }
+
+    public class DeleteCommand : ICommand
+    {
+        private TextEditor _editor;
+        private string _deletedText;
+
+        public DeleteCommand(TextEditor editor, int length) {
+            _editor = editor;
+
+            int start = Math.Max(0, editor.Text.Length - length);
+            _deletedText = editor.Text.Substring(start);
+        }
+
+        public void Execute() {
+            _editor.Delete(_deletedText.Length);
+        }
+
+        public void Undo() {
+            _editor.Insert(_deletedText);
+        }
+    }
+
+    public class EditorInvoker
+    {
+        private Stack<ICommand> _history = new Stack<ICommand>();
+
+        public void ExecuteCommand(ICommand command) {
+            command.Execute();
+            _history.Push(command);
+        }
+
+        public void Undo() {
+            if (_history.Count > 0)
+            {
+                ICommand command = _history.Pop();
+                command.Undo();
+            }
+        }
+    }
+}
---
+++ tests/CommandProject.Tests/TestEditor.cs
@@ -0,0 +1,31 @@
+namespace CommandProject.Tests;
+
+using CommandProject;
+
+public class TestEditor {
+    
+    [Fact]
+    public void TestText() {
+        TextEditor editor = new TextEditor();
+        EditorInvoker invoker = new EditorInvoker();
+
+        invoker.ExecuteCommand(new InsertCommand(editor, "Hello "));
+        invoker.ExecuteCommand(new InsertCommand(editor, "World"));
+        
+        Assert.Equal("Hello World", editor.Text);
+        Console.WriteLine(editor.Text);
+
+        invoker.ExecuteCommand(new DeleteCommand(editor, 5));
+        
+        Assert.Equal("Hello ", editor.Text);
+        Console.WriteLine(editor.Text);
+
+        invoker.Undo();
+        Assert.Equal("Hello World", editor.Text);
+        Console.WriteLine(editor.Text);
+
+        invoker.Undo();
+        Assert.Equal("Hello ", editor.Text);
+        Console.WriteLine(editor.Text);
+    }
+}
